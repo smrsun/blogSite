fc4526f552f36483e7076dc46f9c103a0dbe834c
smrsun4 <smrsun4@gmail.com>
2024-07-09 15:17:46 -0400

    added loop and created elements and appended to the blog div

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # blogSite
+
+## Project Description
+To create a two-page website where users will input and view blog posts, including building a content form, dynamically rendering blog posts
+
+
+### Detail of Work
+
+    *  load an app, where I am presented with the landing page containing a form with labels and inputs for username, blog title, and blog content.
+    *  posted data is stored in localStorage and I am then redirected to the posts page.
+    *  When I view the main content, I am presented with a list of blog posts that are pulled from localStorage.
+    *  When I view localStorage, I am presented with a JSON array of blog post objects, each including the post author's username, title of the post, and post's content.
+    * Commented on changed code and commited to github
+
+
+
+[Link to Blog Site page](http://127.0.0.1:5500/blogSite/index.html)
+
+
+[Link to Github Repo Page](https://github.com/smrsun/)
--- assets/js/blog.js
+++ assets/js/blog.js
@@ -1,8 +1,24 @@
 let blogs = JSON.parse(localStorage.getItem('blogs')) || [];
 
-// loop over the blogs
+// loop over the blogs (look at the activity for create and append)
+const blogsContainer = document.getElementById('blogs');
 
-// in the loop create the elements for the blog div. elements needed for username, title, content.
-// append those elements to a div. append that div to the html(the element with the id of blogs)
+// Loop over the array
 
-// look at the activity for create and append
+blogs.forEach((blog) => {
+  const blogDiv = document.createElement('div');
+  const usernameEl = document.createElement('h2');
+  const titleEl = document.createElement('h3');
+  const contentEl = document.createElement('P');
+
+  usernameEl.textContent = blog.username;
+  titleEl.textContent = blog.title;
+  contentEl.textContent = blog.content;
+
+  blogDiv.appendChild(usernameEl);
+  blogDiv.appendChild(titleEl);
+  blogDiv.appendChild(contentEl);
+
+  // Append blogDiv to the blogsContainer
+  blogsContainer.appendChild(blogDiv);
+});
